25b24fb9b3bdb993f24ee14d70e7b44c6ac28e41
noureddine.najem@gmail.com <noureddine.najem@gmail.com>
2018-05-26 18:29:11 +0000

    override register form fosuserbundle

---
+++ src/AppBundle/Form/RegistrationFormType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use CoreBundle\Entity\Agency;
+
+class RegistrationFormType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('firstName')
+                ->add('lastName')
+                ->add('agency', EntityType::class, array(
+
+                 'class' => 'CoreBundle:Agency',
+                    'choice_label' => 'name'));
+    }
+
+   public function getParent()
+ 
+   {
+       return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+   }
+ 
+   public function getBlockPrefix()
+ 
+   {
+       return 'app_user_registration';
+   }
+ 
+   public function getName()
+ 
+   {
+       return $this->getBlockPrefix();
+   }
+
+
+
+
+}
