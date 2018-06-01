4c392ff7e48e78c413bb48ddde3cbd6be5d764f1
abdelali <rcappv2@gmail.com>
2018-06-01 02:25:42 +0000

    Ajouter un listener pour checker si l'utilisateur a le droit de voir les donnees des autres agences

---
+++ src/CoreBundle/EventListener/EntityPermissionListener.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace CoreBundle\EventListener;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class EntityPermissionListener
+{
+    /**
+     * @var TokenStorageInterface
+     */
+    private $tokenStorage;
+
+    private $excludedEntities = [User::class];
+
+    /**
+     * EntitySetAgencyListener constructor.
+     * @param TokenStorageInterface $tokenStorage
+     */
+    public function __construct(TokenStorageInterface $tokenStorage)
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postLoad(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (in_array(get_class($entity), $this->excludedEntities) || !is_callable([$entity, 'getAgency'])) {
+            return;
+        }
+
+        if (!empty($entity->getAgency())) {
+            /** @var User $user */
+            $user = $this->tokenStorage->getToken()->getUser();
+            if ($user->getAgency()->getId() != $entity->getAgency()->getId()) {
+                throw new NotFoundHttpException();
+            }
+        }
+    }
+}
+
