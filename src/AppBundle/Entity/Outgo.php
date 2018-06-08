413e5e5a0382a648d5b1daa0f3040e7f848ac23e
soldatAppProjectManager <35144231+soldatAppProjectManager@users.noreply.github.com>
2018-06-08 21:04:01 +0000

    update entities (#23)
    * update entities
    
    * self cr

--- src/AppBundle/Entity/Outgo.php
+++ src/AppBundle/Entity/Outgo.php
@@ -5,16 +5,13 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * Outgo
- *
+ * Class Outgo
+ * @package AppBundle\Entity
  *
  * @ORM\Entity()
  */
 class Outgo extends AbstractPayment
 {
     const DISCRIMINATOR = 'outgo';
-
-
-
 }
 
--- src/AppBundle/Entity/Payment.php
+++ src/AppBundle/Entity/Payment.php
@@ -7,79 +7,9 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Payment
  *
- * @ORM\Table(name="payment")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\PaymentRepository")
  */
 class Payment extends  AbstractPayment
 {
-
     const DISCRIMINATOR = 'payment';
-    
-    /**
-     * @ORM\ManyToOne(targetEntity="CoreBundle\Entity\ReferenceModePayment")
-     */
-    private $modePayment;
-
-    /**
-     * @var PaymentDetail
-     *
-     * @ORM\OneToOne(targetEntity="PaymentDetail", cascade={"persist", "remove"}, orphanRemoval=true )
-     */
-    private $detail;
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set modePayment
-     *
-     * @param \CoreBundle\Entity\ReferenceModePayment $modePayment
-     *
-     * @return Payment
-     */
-    public function setModePayment(\CoreBundle\Entity\ReferenceModePayment $modePayment = null)
-    {
-        $this->modePayment = $modePayment;
-        return $this;
-    }
-
-    /**
-     * Get modePayment
-     *
-     * @return \CoreBundle\Entity\ReferenceModePayment
-     */
-    public function getModePayment()
-    {
-        return $this->modePayment;
-    }
-
-    /**
-     * Set detail
-     *
-     * @param \AppBundle\Entity\PaymentDetail $detail
-     *
-     * @return Payment
-     */
-    public function setDetail(\AppBundle\Entity\PaymentDetail $detail = null)
-    {
-        $this->detail = $detail;
-        return $this;
-    }
-
-    /**
-     * Get detail
-     *
-     * @return \AppBundle\Entity\PaymentDetail
-     */
-    public function getDetail()
-    {
-        return $this->detail;
-    }
 }
