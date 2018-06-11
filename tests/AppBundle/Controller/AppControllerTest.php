0591005fc2e125d1ebed2cd66e5c8f82bb271692
Majri Abdelali <abdelali.majri@solutiondata.ma>
2018-06-11 11:28:25 +0000

    test integration

--- tests/AppBundle/Controller/AppControllerTest.php
+++ tests/AppBundle/Controller/AppControllerTest.php
@@ -4,15 +4,15 @@ namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class DefaultControllerTest extends WebTestCase
+class AppControllerTest extends WebTestCase
 {
     public function testIndex()
     {
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+echo $client->getResponse()->getStatusCode();die;
+$this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
 }
